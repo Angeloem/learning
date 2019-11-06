0e4bcf040dd4e1166afa330c7f557a92c1c4344a
angeloem <esanga530@gmail.com>
2019-11-06 00:50:41 -0500

    added an appbar

--- lib/ui_test/car_rent.dart
+++ lib/ui_test/car_rent.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning/model/car.dart';
+import 'package:learning/ui_test/layout.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,7 +22,30 @@ class MainApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
     return Scaffold(
-	   
+	   appBar: AppBar(
+		   backgroundColor: Colors.transparent,
+		   leading: Container(
+			   margin: EdgeInsets.only(left: 25),
+			   child: Icon(
+				   Icons.arrow_back
+			   ),
+		   ),
+		   actions: <Widget>[
+		   	Container(
+			    margin: EdgeInsets.only(right: 25),
+			    child: Icon(Icons.favorite_border),
+		    )
+		   ],
+	   ),
+		backgroundColor: Colors.black,
+	    body: LayoutStart(),
     );
+  }
+}
+
+class LayoutStart extends StatelessWidget{
+	@override
+	Widget build(BuildContext context) {
+    return null;
   }
 }
