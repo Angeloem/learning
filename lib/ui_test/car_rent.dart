62a4d774ec4b63df9aacda0c38294f8b15c7d77c
angeloem <esanga530@gmail.com>
2019-11-07 08:46:14 -0500

    completed the car details class

--- lib/ui_test/car_rent.dart
+++ lib/ui_test/car_rent.dart
@@ -56,7 +56,7 @@ class LayoutStart extends StatelessWidget{
 
 class CarDetailsAnimation extends StatefulWidget{
 	@override
-	_CarDetailsAnimationState createState() => _CarDetailsAnimationState():
+	_CarDetailsAnimationState createState() => _CarDetailsAnimationState();
 }
 
 class _CarDetailsAnimationState extends State<CarDetailsAnimation>{
@@ -64,4 +64,84 @@ class _CarDetailsAnimationState extends State<CarDetailsAnimation>{
 	Widget build(BuildContext context) {
     return CarDetails();
   }
+}
+
+class CarDetails extends StatelessWidget{
+	@override
+	Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+	    child: Column(
+		    crossAxisAlignment: CrossAxisAlignment.start,
+		    children: <Widget>[
+		    	Container(
+				    padding: EdgeInsets.only(left: 30),
+				    child: _carTitle(),
+			    ),
+			    Container(
+				    width: double.infinity,
+				    child: CarCarousel extends StatefulWidget(),
+			    )
+		    ],
+	    ),
+    );
+  }
+  
+  _carTitle(){
+		return Column(
+			crossAxisAlignment: CrossAxisAlignment.start,
+			children: <Widget>[
+				RichText(
+					text: TextSpan(
+						style: TextStyle(
+							color: Colors.white,
+							fontSize: 38
+						),
+						children: [
+							TextSpan(text: currentCar.companyName),
+							TextSpan(text: '\n'),
+							TextSpan(text: currentCar.carName, style: TextStyle(
+								fontWeight: FontWeight.w700
+							)),
+						]
+					),
+				),
+				SizedBox(height: 10,),
+				RichText(
+					text: TextSpan(
+						style: TextStyle(
+							fontSize: 16
+						),
+						children: [
+							TextSpan(
+								text: currentCar.price.toString(),
+								style: TextStyle(
+									color: Colors.grey[20]
+								),
+							),
+							TextSpan(
+								text: '/ day',
+								style: TextStyle(
+									color: Colors.grey
+								)
+							)
+						]
+					),
+				),
+			],
+		);
+  }
+}
+
+class CarCarousel extends StatefulWidget {
+	@override
+	CarCarouselState createState() => CarCarouselState();
+}
+
+class CarCarouselState extends State<CarCarousel> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+	
 }
